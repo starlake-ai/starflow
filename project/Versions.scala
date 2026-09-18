556bb85204294e2dfcd3c0622ea3aca4fc820d97
Hayssam Saleh <hayssam@saleh.fr>
2026-09-18 17:19:23 +0200

    Update Jsqltranspiler

--- project/Versions.scala
+++ project/Versions.scala
@@ -55,7 +55,7 @@ object Versions {
     "10.15.2.0" // last version compatible with Java 11, see https://db.apache.org/derby/derby_downloads.html
   // jSqlParser must match the version jsqltranspiler's pom pins
   val jSqlParser = "5.4.15"
-  val jSqlTranspiler = "1.12"
+  val jSqlTranspiler = "1.13"
   val starlakejdbc = "0.7"
   val airflowTemplates = "0.6.17"
   val jSqlFormatter = "5.4.1.1"
